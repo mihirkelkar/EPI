b00a7cbcabca0fabbb66a0a3207fbe0f909c58ed
mihirkelkar <kelkarmhr@gmail.com>
2015-12-08 07:11:12 -0600

    Adding code

---
+++ List_And_Arrays/reverseListKNodeAlternate/README.md
@@ -0,0 +1,9 @@
+Write a function which takes a list as an input and then reverses k nodes at a time. If the list does not have nodes which are a multiple of k, the last n mod k nodes are left unchanged. 
+
+1) Count the number of nodes in list. 
+
+2) Start reversing k nodes by a counter. 
+
+3) Once that is done. if count > k. Traverse k stragiht.
+
+4) If count > k. Reverse.
---
+++ List_And_Arrays/reverseListKNodeAlternate/reverseAlterante.py
@@ -0,0 +1,38 @@
+#Need to check if code is correct
+def reverseAlternate(llist):
+  curr = llist.head
+  listlen = 0
+  while curr != None:
+    listlen += 1
+    curr = curr.next
+  curnode = llist.head
+  prev = None
+  temp = curnode.next
+  remNodes = listlen
+  while True:
+    if remNodes >= k:
+      num = k
+      firstNode = curnode
+      while num > 0:
+        temp = curnode.next
+        curnode.next = prev
+        prev = curnode
+        curnode = temp
+        remNodes -= 1
+        num -= 1
+      firstNode.next = curnode
+      #This will fire only for the first inversion
+      if remNodes == listlen - k:
+        llist.head = prev
+      if remNodes != 0:
+        num = k
+        while curnode != None and num > 0:
+          prev = curnode
+          curnode = curnode.next
+          remNodes -= 1
+        if num > 0:
+          self.tail = prev
+      else:
+        self.tail = firstNode  
+       
+    
