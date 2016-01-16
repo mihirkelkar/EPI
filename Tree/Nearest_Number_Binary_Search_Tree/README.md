1337877327162961db2eac2521c8ac06900f1323
mihirkelkar <kelkarmhr@gmail.com>
2016-01-16 10:41:02 -0600

    might have bugs

---
+++ Tree/Nearest_Number_Binary_Search_Tree/README.md
@@ -0,0 +1,1 @@
+Given a number, find the closest number to it that is less than or equal to the number itself. 
---
+++ Tree/Nearest_Number_Binary_Search_Tree/nearestNum.py
@@ -0,0 +1,66 @@
+class Node(object):
+  def __init__(self, value):
+    self.value = value
+    self.left = None
+    self.right = None
+  
+class BinarySearchTree(object):
+  def __init__(self):
+    self.root = None
+    self.nearLess = None
+  def addNode(self, value, root=None):
+    root = self.root if root is None else root
+    if root == None:
+      self.root = Node(value)
+    else:
+      if value <= root.value:
+        if root.left == None:
+          root.left = Node(value)
+        else:
+          self.addNode(value, root.left)
+      else:
+        if root.right == None:
+          root.right = Node(value)
+        else:
+          self.addNode(value, root.right)
+
+  def findNearestLeft(self, value, root=None):
+    root = self.root if root is None else root
+    if root == None:
+      return None
+    else:
+      if value == root.value:
+        self.nearLess = value
+      elif value < root.value:
+        if root.left != None:
+          self.findNearestLeft(value, root.left)
+        else:
+          return None
+      elif value > root.value:  
+        if root.right != None:
+          self.findNearestLeft(value, root.right)
+        else:
+          if self.nearLess != None:
+            if root.value >= self.nearLess:
+              self.nearLess = root.value
+          else:
+            self.nearLess = root.value 
+
+
+
+def main():
+  tree = BinarySearchTree()
+  tree.addNode(10)
+  tree.addNode(9)
+  tree.addNode(11)
+  tree.addNode(9.5)
+  tree.addNode(8.5)
+  tree.addNode(10.5)
+  tree.addNode(11.5)
+  print tree.root.left.left.value
+  print tree.root.right.right.value
+  tree.findNearestLeft(8.75)
+  print tree.nearLess
+
+if __name__ == "__main__":
+  main()
